3236f5d3a7107074926a7f9fae3e0292a5015eb5
monbostest <52292370+monbostest@users.noreply.github.com>
2022-04-16 09:48:06 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "name" {
+  
+}
+
+variable "account_tier" {
+    default = "Standard"
+  
+}
+
+variable "account_replication_type" {
+  default = "GRS"
+}
+
+
+variable "resource_group_name" {
+    default = "example-resources-dev"
+  
+}
+
+variable "location" {
+    default = "EAST US 2"
+  
+}
