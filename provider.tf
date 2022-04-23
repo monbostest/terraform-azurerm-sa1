435edae8bbf484a415485f43f32691230bb0792d
monbostest <52292370+monbostest@users.noreply.github.com>
2022-04-23 21:03:29 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
   features {}
   subscription_id = "0c6b065f-5b32-47b7-b55f-6e6cbe2eb88c"
-  client_id       = "82603bb8-eb23-4e9e-9d0e-1d558ca46fba"
-  client_secret   = "g6U7Q~GDLOrTvdqbB2lQxIOQsha2rSJjx6KuZ"
+  client_id       = var.client_id
+  client_secret   = var.client_secret 
   tenant_id       = "a01e7d9a-780e-4241-8f14-e897e5568cbf"
 }
